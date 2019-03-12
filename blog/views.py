a996f39d64cca25bfe54b6eab5fcf410d7ef6917
nion8 <nion8@mail.ru>
2019-03-12 13:58:41 +0300

    names in index

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
+
+def posts_list(request):
+    n=['Олег','маша','Вика', 'Муська']
+    return render(request,'blog/index.html', context={'names': n})
