c225777cd2386b371b421d4d7f2276477a02c78a
nion8 <nion8@mail.ru>
2019-03-12 18:07:15 +0300

    urls on blog

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+
+
 # from django.http import HttpResponse
 
 # Create your views here.
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request,'blog/index.html', context={'posts': posts})
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
